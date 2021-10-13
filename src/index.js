0747b4911579873eb281ea8749fe39ae75fc677e
ktakaue <75979960+ktakaue@users.noreply.github.com>
2021-10-13 23:40:00 +0900

    復習(var,let,constの違い)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import "./styles.css";
+//
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "Var変数を上書き";
+// console.log(val1);
+
+// //
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+const val3 = "const変数";
+console.log(val3);
+// val3 = "const変数を上書き"
