746f10920b9e6175998a5e0e10187eda6729f483
ktakaue <75979960+ktakaue@users.noreply.github.com>
2021-10-14 14:01:56 +0900

    map and filter

--- src/index.js
+++ src/index.js
@@ -116,3 +116,30 @@
 // arr6[0] = 100;
 // console.log(arr6);
 // console.log(arr4);
+
+const nameArr = ["田中", "山田", "じゃけえ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です。`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}だよ`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "じゃけえ") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr);
