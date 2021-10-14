02caaf776d570271b2dddc13fbce50a567759d41
ktakaue <75979960+ktakaue@users.noreply.github.com>
2021-10-14 13:44:45 +0900

    スプレット構文

--- src/index.js
+++ src/index.js
@@ -1,65 +1,118 @@
-// // //
-
-// // // var val1 = "var変数";
-// // // console.log(val1);
-
-// // // // var変数は上書き可能
-// // // val1 = "Var変数を上書き";
-// // // console.log(val1);
-
 // // // //
-// // // var val1 = "var変数を再宣言";
-// // // console.log(val1);
 
-// // // let val2 = "let変数";
-// // // console.log(val2);
+// // // // var val1 = "var変数";
+// // // // console.log(val1);
 
-// // // // letは上書きが可能
-// // // val2 = "let変数を上書き";
-// // // console.log(val2);
+// // // // // var変数は上書き可能
+// // // // val1 = "Var変数を上書き";
+// // // // console.log(val1);
 
-// // const val3 = "const変数";
-// // console.log(val3);
-// // const val4 = {
-// //   name: "じゃけぇ",
-// //   age: 28
+// // // // //
+// // // // var val1 = "var変数を再宣言";
+// // // // console.log(val1);
+
+// // // // let val2 = "let変数";
+// // // // console.log(val2);
+
+// // // // // letは上書きが可能
+// // // // val2 = "let変数を上書き";
+// // // // console.log(val2);
+
+// // // const val3 = "const変数";
+// // // console.log(val3);
+// // // const val4 = {
+// // //   name: "じゃけぇ",
+// // //   age: 28
+// // // };
+// // // val4.name = "jak";
+// // // val4.address = "Hiroshima";
+// // // console.log(val4);
+
+// // // const val5 = ["dog", "cat"];
+// // // val5[0] = "bird";
+// // // val5.push("monkey");
+// // // console.log(val5);
+
+// // // テンプレート文字列
+
+// // const name = "じゃけえ";
+// // const age = 28;
+
+// // // 従来の方法
+// // // const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+// // // console.log(message1);
+
+// // // テンプレート文字列を用いた方法
+// // const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// // console.log(message2);
+
+// // アロー関数
+// // function func1(str) {
+// //   return str;
+// // }
+// // const func1 = function (str) {
+// //   return str;
 // // };
-// // val4.name = "jak";
-// // val4.address = "Hiroshima";
-// // console.log(val4);
+// // console.log(func1("func1です"));
 
-// // const val5 = ["dog", "cat"];
-// // val5[0] = "bird";
-// // val5.push("monkey");
-// // console.log(val5);
+// // アロー関数
+// // const func2 = (str) => str;
 
-// // テンプレート文字列
+// // console.log(func2("func2です"));
 
-// const name = "じゃけえ";
-// const age = 28;
+// // const func3 = (num1, num2) => num1 + num2;
 
-// // 従来の方法
-// // const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+// // console.log(func3(10, 20));
+
+// const myProfile = {
+//   name: "じゃけえ",
+//   age: 28
+// };
+
+// // const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+
 // // console.log(message1);
 
-// // テンプレート文字列を用いた方法
-// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+
 // console.log(message2);
 
-// アロー関数
-// function func1(str) {
-//   return str;
-// }
-// const func1 = function (str) {
-//   return str;
-// };
-// console.log(func1("func1です"));
+// const myProfile = ["じゃけえ", 28];
+// // const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// // console.log(message3);
 
-// アロー関数
-// const func2 = (str) => str;
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
 
-// console.log(func2("func2です"));
+// default値
 
-const func3 = (num1, num2) => num1 + num2;
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+// sayHello();
 
-console.log(func3(10, 20));
+// スプレッド構文
+// 配列の展開
+
+// const arr1 = [1, 2, 3];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2, num3) => console.log(num1 + num2 + num3);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// // まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
