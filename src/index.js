5de85a5a952b813bba40f21f8c44456819d03bac
ktakaue <75979960+ktakaue@users.noreply.github.com>
2021-10-14 11:06:36 +0900

    テンプレート文字列追加

--- src/index.js
+++ src/index.js
@@ -1,34 +1,49 @@
-//
+// // //
 
-// var val1 = "var変数";
-// console.log(val1);
+// // // var val1 = "var変数";
+// // // console.log(val1);
 
-// // var変数は上書き可能
-// val1 = "Var変数を上書き";
-// console.log(val1);
+// // // // var変数は上書き可能
+// // // val1 = "Var変数を上書き";
+// // // console.log(val1);
 
-// //
-// var val1 = "var変数を再宣言";
-// console.log(val1);
+// // // //
+// // // var val1 = "var変数を再宣言";
+// // // console.log(val1);
 
-// let val2 = "let変数";
-// console.log(val2);
+// // // let val2 = "let変数";
+// // // console.log(val2);
 
-// // letは上書きが可能
-// val2 = "let変数を上書き";
-// console.log(val2);
+// // // // letは上書きが可能
+// // // val2 = "let変数を上書き";
+// // // console.log(val2);
 
-const val3 = "const変数";
-console.log(val3);
-const val4 = {
-  name: "じゃけぇ",
-  age: 28
-};
-val4.name = "jak";
-val4.address = "Hiroshima";
-console.log(val4);
+// // const val3 = "const変数";
+// // console.log(val3);
+// // const val4 = {
+// //   name: "じゃけぇ",
+// //   age: 28
+// // };
+// // val4.name = "jak";
+// // val4.address = "Hiroshima";
+// // console.log(val4);
 
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// // const val5 = ["dog", "cat"];
+// // val5[0] = "bird";
+// // val5.push("monkey");
+// // console.log(val5);
+
+// // テンプレート文字列
+
+// const name = "じゃけえ";
+// const age = 28;
+
+// // 従来の方法
+// // const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+// // console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// アロー関数
