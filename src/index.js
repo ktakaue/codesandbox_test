3f388ba441b56e294669347aa3de9b7b63f44740
ktakaue <75979960+ktakaue@users.noreply.github.com>
2021-10-14 12:43:15 +0900

    アロー関数追加

--- src/index.js
+++ src/index.js
@@ -47,3 +47,19 @@
 // console.log(message2);
 
 // アロー関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// アロー関数
+// const func2 = (str) => str;
+
+// console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+
+console.log(func3(10, 20));
