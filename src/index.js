b34ba29c95dd66680093c231a3c04b40ddc90c61
ktakaue <75979960+ktakaue@users.noreply.github.com>
2021-10-15 19:57:34 +0900

    三項演算子追加

--- src/index.js
+++ src/index.js
@@ -135,11 +135,48 @@ const nameArr = ["田中", "山田", "じゃけえ"];
 // });
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "じゃけえ") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけえ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+// 三項演算子
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+
+// console.log(val1);
+
+// const num = "1300";
+
+// const formattedNum =
+//   typeof num === `number` ? num.toLocaleString() : `数値を入力してください`;
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? `100を超えています。` : `許容範囲内です。`;
+// };
+
+// console.log(checkSum(50, 50));
+
+// 論理演算子の本当の意味をしろう
+
+// const flag1 = false;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("１も２もtrueになります");
+// }
+
+// 左側がfalse判定の場合、右側（左側がTrueの場合、左側を返す）
+
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// 左側がtrueなら右側を返す
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
